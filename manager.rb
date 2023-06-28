befd686922aae9924e4eb02a3f506d821277fbe8
“Nickhil <suryanscrit@gmail.com>
2023-06-28 14:25:50 -0500

    fire all employees and give annual raise method loops

--- manager.rb
+++ manager.rb
@@ -1,17 +1,79 @@
-class StoreItem
-  attr_accessor :name, :color, :price
+class Employee
+  attr_reader :first_name, :last_name, :active
+  attr_writer :active
 
-  def initialize(options)
-    @name = options[:name]
-    @color = options[:color]
-    @price = options[:price]
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
+  end
+
+  def print_info
+    puts "#{first_name} #{last_name} makes #{@salary} dollars a year."
+  end
+
+  def give_annual_raise
+    @salary = 1.07 * @salary
   end
 end
 
-shirt = StoreItem.new(name: "Shirt", color: "Blue", price: 20.00)
-pants = StoreItem.new(name: "Pants", color: "Black", price: 30.00)
-hat = StoreItem.new(name: "Hat", color: "Red", price: 15.00)
+employee1 = Employee.new({ first_name: "Majora", last_name: "Carter", salary: 80000, active: true })
+employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
+employee1.print_info
+employee2.print_info
 
-p shirt.name
-p hat.color
-p pants.price
+class Manager < Employee
+  def initialize(input_options)
+    super
+    @employees = input_options[:employees]
+  end
+
+  def send_report
+    puts "Sending email..."
+    # use email sending library
+    puts "email sent!"
+  end
+end
+
+manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
+p manager
+manager.print_info
+manager.send_report
+
+
+# Create a method in the Manager class called give_all_raises that loops through each of the manager’s employees and gives them a raise (using the give_annual_raise method). Demonstrate how it works.
+# Create a method in the Manager class called fire_all_employees that loops through each of the manager’s employees and changes their active status to false.
+# Bonus: What happens when you define a method in the Employee and Manager class with the same name? Read more about method overriding here: https://medium.com/@mattrice12/method-overriding-and-super-f19cf7274c4. Then try to use super on a method other than initialize!
+
+
+
+class Manager < Employee
+  def initialize(input_options)
+    super
+    @employees = input_options[:employees]
+  end
+
+  def send_report
+    puts "Sending email..."
+    # use email sending library
+    puts "email sent!"
+  end
+
+  def give_all_raises
+    @employees.each do |employee|
+      employee.give_annual_raise
+    end
+  end
+
+  def fire_all_employees
+    @employees.each do |employee|
+      employee.active = false
+    end
+  end
+end
+
+manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
+manager.give_all_raises
+manager.fire_all_employees
+
