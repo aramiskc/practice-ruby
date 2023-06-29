7f3f94fb422621e356efe1514a30a05f1bc4adc5
“Nickhil <suryanscrit@gmail.com>
2023-06-29 12:30:58 -0500

    changed class to module for StoreItem

--- store_item.rb
+++ store_item.rb
@@ -35,8 +35,28 @@
 #  p shirt.name
 #  p hat.color
 #  p pants.price
+# =======================================================================================
 
-class StoreItem
+
+# class StoreItem
+#   attr_accessor :name, :color, :price
+
+#   def initialize(options)
+#     @name = options[:name]
+#     @color = options[:color]
+#     @price = options[:price]
+#   end
+# end
+
+# shirt = StoreItem.new(name: "Shirt", color: "Blue", price: 20.00)
+# pants = StoreItem.new(name: "Pants", color: "Black", price: 30.00)
+# hat = StoreItem.new(name: "Hat", color: "Red", price: 15.00)
+
+# p shirt.name
+# p hat.color
+# p pants.price
+  
+module StoreItem
   attr_accessor :name, :color, :price
 
   def initialize(options)
@@ -46,6 +66,8 @@ class StoreItem
   end
 end
 
+include StoreItem
+
 shirt = StoreItem.new(name: "Shirt", color: "Blue", price: 20.00)
 pants = StoreItem.new(name: "Pants", color: "Black", price: 30.00)
 hat = StoreItem.new(name: "Hat", color: "Red", price: 15.00)
@@ -53,4 +75,3 @@ hat = StoreItem.new(name: "Hat", color: "Red", price: 15.00)
 p shirt.name
 p hat.color
 p pants.price
-  
